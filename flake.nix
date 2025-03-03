9e9dbd4d41741d53844cf7693eb5a4399e0dbcdf
Andy Lee <andylizf@outlook.com>
2025-03-03 15:40:21 +0800

    chore: flake.nix with modern-cv package

--- flake.nix
+++ flake.nix
@@ -10,6 +10,20 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        
+        # Create a derivation for modern-cv package
+        modern-cv-pkg = pkgs.stdenv.mkDerivation {
+          name = "modern-cv";
+          version = "0.7.0";
+          src = ./modern-cv;
+          
+          buildPhase = "true"; # No build needed
+          
+          installPhase = ''
+            mkdir -p $out/packages/local/modern-cv/0.7.0
+            cp -r ./* $out/packages/local/modern-cv/0.7.0/
+          '';
+        };
       in
       {
         devShells.default = pkgs.mkShell {
@@ -21,6 +35,12 @@
             source-sans-pro
             font-awesome
           ];
+          
+          shellHook = ''
+            export TYPST_HOME="${modern-cv-pkg}"
+            export TYPST_ROOT="$PWD"
+            exec ${pkgs.fish}/bin/fish
+          '';
         };
       }
     );
